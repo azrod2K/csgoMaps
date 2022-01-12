34183c6d6ace8ceebad579f26b215ac4c58b5a51
azrod2K <61694504+azrod2K@users.noreply.github.com>
2022-01-12 23:48:49 +0100

    update

--- csgomaps/vues/map.php
+++ csgomaps/vues/map.php
@@ -3,10 +3,15 @@
 ?>
 <div class="container">
     <div class="row">
-
         <img src="assets/img/<?= $map->getImg() ?>.jpg" style="width: 100%">
         <h1><?= $map->getNm_Maps() ?></h1>
-        
     </div>
-        <h3><?= $map->getTxt_Descritpion() ?></h3>
+    <h3><?= $map->getTxt_Descritpion() ?></h3>
+    <br>
+    <h5>Stratégie pour ct</h5>
+    <p>rester sur le site</p>
+    <p>...</p>
+    <h5>stratégie pour t</h5>
+    <p>rush b on s'arrête pas rush!!</p>
+    <p>...</p>
 </div>
